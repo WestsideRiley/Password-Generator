9decd8b9087ff80a6f505c5feeae1b2c76c906b0
Wesley Riley <Wesley.A.Riley@gmail.com>
2022-05-08 02:26:45 -0500

    Addition of functional selection criteria within the Javascript. Prompt input can be interpreted

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -39,6 +39,7 @@ function generatePassword(passwordLength) {
           alert("thank you");
         }; 
     }
+
   var containsNumbers= prompt("Should this password contain numbers?");
     
     while (containsNumbers !== "yes" && containsNumbers !== "no" ) {
@@ -68,29 +69,79 @@ function generatePassword(passwordLength) {
           containsSpecial=prompt("Please respond with yes or no")     
         }
       }
+
+var numbers = "0123456789";
+  var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+  var Special="!#$%&*-<>?@^"
+  const allChars = lowerCase + numbers + upperCase + Special;
+  const LowerOnly=lowerCase;
+  const NumberOnly= lowerCase + numbers;
+  const UpperOnly= lowerCase + upperCase;
+  const SpecialOnly= lowerCase + Special;
+  const NumUpper = lowerCase + numbers + upperCase;
+  const NumSpec = lowerCase + numbers + Special;
+  const UpperSpec= lowerCase + upperCase + Special;
+ 
+
+  var randPasswordArray = Array(passwordLength);
+  randPasswordArray[0] = numbers;
+  randPasswordArray[1] = upperCase;
+  randPasswordArray[2] = lowerCase;
+  randPasswordArray[3] = Special;
+  randPasswordArray = randPasswordArray.fill(PasswordCriteria());
+
+      function PasswordCriteria(){
+         
+        if(containsUpper==="yes" && containsNumbers==="yes" && containsSpecial==="yes"){
+           return allChars;    
+        }
+
+        if(containsUpper==="no" && containsNumbers==="no" && containsSpecial==="no"){
+           return LowerOnly;
+        }
+
+        if(containsUpper==="no" && containsNumbers==="yes" && containsSpecial==="no"){
+           return NumberOnly;
+        }
+
+        if(containsUpper==="yes" && containsNumbers==="no" && containsSpecial==="no"){
+           return UpperOnly;
+        }
+
+        if(containsUpper==="no" && containsNumbers==="no" && containsSpecial==="yes"){
+           return SpecialOnly;  
+        }
+
+        if(containsUpper==="yes" && containsNumbers==="yes" && containsSpecial==="no"){
+          return NumUpper;   
+        }
+
+        if(containsUpper==="no" && containsNumbers==="yes" && containsSpecial==="yes"){
+          return NumSpec;
+        }
+
+        if(containsUpper==="yes" && containsNumbers==="no" && containsSpecial==="yes"){
+          return UpperSpec;    
+        }
+
+      }
+
+  return shuffleArray(randPasswordArray.map(function(x) { return x[Math.floor(Math.random() * x.length)] })).join('');
+
+
+function shuffleArray(array) {
+  for (var i = array.length - 1; i > 0; i--) {
+    var j = Math.floor(Math.random() * (i + 1));
+    var temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
+  return array;
 }
 
       
-  var numbers = "0123456789";
-  var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  var lowerCase = "abcdefghijklmnopqrstuvwxyz";
-  var Special="!#$%&*+-<=>?@^"
-  var allChars = numbers + upperCase + lowerCase + Special;
-  //var randPasswordArray = Array(passwordLength);
-  //randPasswordArray[0] = numbers;
-  //randPasswordArray[1] = upperCase;
-  //randPasswordArray[2] = lowerCase;
-  //randPasswordArray[3] = Special;
-  //randPasswordArray = randPasswordArray.fill(allChars, 10);
-  //return shuffleArray(randPasswordArray.map(function(x) { return x[Math.floor(Math.random() * x.length)] })).join('');
-//}
+}
 
-//function shuffleArray(array) {
-  //for (var i = array.length - 1; i > 0; i--) {
-    //var j = Math.floor(Math.random() * (i + 1));
-    //var temp = array[i];
-    //array[i] = array[j];
-    //array[j] = temp;
-  //}
-  //return array;
-//}
+      
+  
